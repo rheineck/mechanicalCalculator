1651d9b5fe76602824c95dc941f54a5eba09f2d9
Raphael <raphaelheineck@gmail.com>
2023-12-08 18:03:43 -0300

    changes in main.js

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
-let paperStack = parseFloat(document.querySelector('.paper').textContent)
-let stack = [];
-let operands = ["/", "*", "-", "+"]
+let paperStack = parseFloat(document.querySelector(".paper").textContent)
+let enterButton = document.getElementById("enter")
+
+let stackNumbers = [];
+let operators = ["/", "*", "-", "+"]
 let displayValue = ""
 
-console.log(paperStack)
-console.log(stack.push(paperStack))
+// ----- Debug -----
+
+console.log(calculate([1, 2, 3, '+', 2, '*', '-']))
+
+// -----------------
+
+function addToPaper() {
+
+  stackNumbers.push(paperStack)
+
+  for (let i = 0; i < stackNumbers.length; i++) {
+    let lastNumber = document.createElement('span')
+    lastNumber.value = `${stackNumbers[i]}`
+  }
+
+  console.log(stackNumbers)
+}
 
 function calculate(arr) {
   for (let i = 0; i < arr.length; i++) {
-    if (operands.includes(arr[i])) {
-      let b = stack.pop()
-      let a = stack.pop()
+    if (operators.includes(arr[i])) {
+      let b = stackNumbers.pop()
+      let a = stackNumbers.pop()
 
-      arr[i] === "*" ? stack.push(a * b) :
-        arr[i] === "/" ? stack.push * (a / b) :
-          arr[i] === "+" ? stack.push(a + b) :
-            stack.push(a - b)
+      arr[i] === "*" ? stackNumbers.push(a * b) :
+        arr[i] === "/" ? stackNumbers.push * (a / b) :
+          arr[i] === "+" ? stackNumbers.push(a + b) :
+            stackNumbers.push(a - b)
     }
-    else stack.push(arr[i])
+    else stackNumbers.push(arr[i])
 
   }
-  return stack
+  return stackNumbers
 }
 
 function appendToDisplay(value) {
@@ -31,5 +48,3 @@ function appendToDisplay(value) {
 function updateDisplay() {
   document.getElementById("paper").value = displayValue
 }
-
-console.log(calculate([1, 2, 3, '+', 2, '*', '-']))
